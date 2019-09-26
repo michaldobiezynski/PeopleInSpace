2c7b8e430170bc5ad0c9dbba0b2715de96bc715c
dobimi <michal.dobiezynski@woodmac.com>
2019-09-26 14:15:18 +0100

    made getJson function a higher order function by adding a callback method to it

--- js/callbacks.js
+++ js/callbacks.js
@@ -4,13 +4,13 @@ const peopleList = document.getElementById('people');
 const btn = document.querySelector('button');
 
 // Make an AJAX request
-function getJSON(url) {
+function getJSON(url, callback) {
   const xhr = new XMLHttpRequest();
   xhr.open('GET', url);
   xhr.onload = () => {
     if(xhr.status === 200) {
       let data = JSON.parse(xhr.responseText);
-      console.log(data);
+      return callback(data);
     }
   };
   xhr.send();
