e9f2fd4a5fa836d9975bcd49891a1f6245787b54
Michal Dobiezynski <michaldobiezynski@hotmail.com>
2019-09-26 18:03:34 +0100

    added a catch to handle a rejected promise

--- js/promises-breakfast.js
+++ js/promises-breakfast.js
@@ -1,9 +1,11 @@
 
 const breakfastPromise = new Promise( (resolve, reject) => {
     setTimeout(() => {
-        resolve('Your order is ready. Come and get it!');
+        // resolve('Your order is ready. Come and get it!');
+        reject('Oh No! There was a problem with your order!');
     }, 3000)
 });
 
 console.log(breakfastPromise);
-breakfastPromise.then(  val => console.log(val));
+breakfastPromise.then(  val => console.log(val))
+    .catch( err => console.log(err));
