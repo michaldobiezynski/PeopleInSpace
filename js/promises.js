16a1292dde3ad91a021fe39a5bf7b5120302bc91
Michal Dobiezynski <michaldobiezynski@hotmail.com>
2019-09-26 19:21:54 +0100

    changed a function to make it work with a promise

--- js/promises.js
+++ js/promises.js
@@ -4,15 +4,17 @@ const peopleList = document.getElementById('people');
 const btn = document.querySelector('button');
 
 function getJSON(url, callback) {
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', url);
-  xhr.onload = () => {
-    if(xhr.status === 200) {
-      let data = JSON.parse(xhr.responseText);
-      return callback(data);
-    }
-  };
-  xhr.send();
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.onload = () => {
+      if(xhr.status === 200) {
+        let data = JSON.parse(xhr.responseText);
+        return callback(data);
+      }
+    };
+    xhr.send();
+  });
 }
 
 function getProfiles(json) {
